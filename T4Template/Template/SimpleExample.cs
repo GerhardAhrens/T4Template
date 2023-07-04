e3d67d1437e840009e0345cdc4be7c779b905f5e
Gerhard Ahrens <gerhard.ahrens@pta.de>
2023-07-04 15:42:29 +0200

    Add SimpleExample

---
+++ T4Template/Template/SimpleExample.cs
@@ -0,0 +1,29 @@
+﻿
+//-----------------------------------------------------------------------
+// <copyright file="TestClass.cs" company="Lifeprojects.de">
+//     Class: TestClass
+//     Copyright © Lifeprojects.de 2023
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>04.07.2023</date>
+//
+// <summary>
+// Class for
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace TestProject.Class
+{
+    using System;
+    using System.Globalization;
+
+    public class TestClass
+    {
+        public TestClass()
+        {
+        }
+    }
+}
+ 
