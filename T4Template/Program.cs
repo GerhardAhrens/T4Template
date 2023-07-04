647dd6d3217d88615e55f42a98b21178457908f3
Gerhard Ahrens <gerhard.ahrens@pta.de>
2023-07-04 15:24:02 +0200

    Projektdateien hinzufügen.

---
+++ T4Template/Program.cs
@@ -0,0 +1,26 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Lifeprojects.de">
+//     Class: Program
+//     Copyright © Lifeprojects.de 2023
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>12.02.2023 09:18:06</date>
+//
+// <summary>
+// Konsolen Applikation
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace T4Template
+{
+    using System;
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
